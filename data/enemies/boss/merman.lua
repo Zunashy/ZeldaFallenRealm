46043c913ea2b0913b9a3e8c16a7a4df5900dc6b
TwilCynder <twilcynder@gmail.com>
2020-01-31 04:18:19 +0100

    Boss #2

--- data/enemies/boss/merman.lua
+++ data/enemies/boss/merman.lua
@@ -12,20 +12,20 @@ local enemy = ...
 local game = enemy:get_game()
 local map = enemy:get_map()
 local hero = map:get_hero()
-local sprite
+local sprite, trident_sprite
 local movement
 
 local mov_speed = 64
 local wave_amp, wave_period = 16, 32
 
-local base_y, travel, last_x = 0, 0, 0
+local base_y, travel, last_x, m_direction = 0, 0, 0
 
 local function base_movement_obstacle_cb(mov)
   mov:stop()
   local x = enemy:get_position()
   travel = travel + (math.abs(last_x - x))
   last_x = x
-  enemy:start_movement(1 - mov.direction, true)
+  enemy:start_movement(1 - m_direction, true)
 end
 
 local function wave()
@@ -41,7 +41,7 @@ function enemy:start_movement(direction, bounce)
   local m = sol.movement.create("straight")
   m:set_speed(mov_speed)
   m:set_angle(math.pi * direction)
-  m.direction = direction
+  m_direction = direction
   m.on_obstacle_reached = base_movement_obstacle_cb
   m.on_position_changed = wave
   if not bounce then
@@ -60,6 +60,23 @@ function enemy:on_created()
   wave_period = wave_period / math.pi
 end
 
+function enemy:swing()
+  local m = self:get_movement()
+  if m then
+    m:stop()
+  end
+  sprite:set_animation("swing_load", function()
+    sprite:set_animation("swing", function()
+      sprite:set_animation("walking")
+      enemy:start_movement(m_direction, true)
+    end)
+  end)
+  trident_sprite:set_animation("swing_load", "swing")
+end
+
 function enemy:on_restarted()
   self:start_movement(0)
+  sol.timer.start(self, 1000, function()
+    enemy:swing()
+  end)
 end
