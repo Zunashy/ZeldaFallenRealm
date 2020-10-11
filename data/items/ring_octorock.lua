582bcc09f87720fbaf10c35ceb3709097a216c4b
Zunashy <guyguyshzunashy@gmail.com>
2020-10-11 17:48:35 +0200

    heart boss / couche donjon 1 / buissons north beach

--- data/items/ring_octorock.lua
+++ data/items/ring_octorock.lua
@@ -4,5 +4,6 @@ local game = item:get_game()
 -- Event called when all items have been created.
 function item:on_started()
   self:set_assignable(false)
+  self:set_savegame_variable("ring_octorok")
   self:set_brandish_when_picked(true)
 end
