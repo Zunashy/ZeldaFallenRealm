76355cd0c5a075ecb3c4bcd296bbc7145b726315
Bertrand-A <alonso.bertrand@orange.fr>
2020-10-26 20:45:31 +0100

    speedy git

--- .git.sh
+++ .git.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 function git_push(){
-git add .
-git commit -a -m "$1"
-git pull && git push origin master
+#git add .
+#git commit -a -m "$1"
+echo "$1"
+#git pull
+#git push origin master
 }
