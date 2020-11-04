db0a2a09f51964ca0dc1b8180cc6c2bd69e37aae
Bertrand-A <alonso.bertrand@orange.fr>
2020-11-04 21:13:47 +0100

    check dat files with dat  in BASH

--- git.sh
+++ git.sh
@@ -1,4 +1,23 @@
 #! /bin/bash
+RED="\e[91m"
+RESET="\e[39m"
+GREEN="\e[92m"
+BOLD="\e[1m"
+NORMAL="\e[0m"
+
+
+function dat(){
+cd "${1:-.}" || exit 1
+for file in $(find . -name "*.dat");
+do
+   if grep -q "<<<<<<< HEAD" $file; then
+         echo -e "[ $RED KO $RESET ] $BOLD $file $NORMAL"
+   else
+         echo -e "[ $GREEN OK $RESET ] $BOLD $file $NORMAL"
+   fi
+done
+}
+
 
 function gpush(){
 git add .
