d9c7c9a90961188e6f13f980474de5d2186c8deb
Bertrand-A <alonso.bertrand@orange.fr>
2020-11-18 14:55:39 +0100

    print me now

--- data/scripts/feature/map_generic.lua
+++ data/scripts/feature/map_generic.lua
@@ -22,11 +22,12 @@ local parse_event_string = require("scripts/feature/event_string")
 
 --Enable an entity with a specified name on the specified map (or disables it, depending on state)
 function map:enable_entity(name)
+
   self = self or sol.main.game:get_map()
   local prop, force
   
   for e in self:get_entities(name) do
-  
+
     force = e:get_property("force_spawn")
     
     if force or not (
