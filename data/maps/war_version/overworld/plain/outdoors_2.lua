5ba5be9b1a60c9325694d59bc76b1fb19a84a03a
Nillan <nillan.eldaar@lilo.org>
2020-11-19 11:10:27 +0100

    plaine guerre

--- data/maps/war_version/overworld/plain/outdoors_2.lua
+++ data/maps/war_version/overworld/plain/outdoors_2.lua
@@ -11,8 +11,8 @@ local map = ...
 local game = map:get_game()
 
 -- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
-
+function map:on_started_()
+self:init_reset_separators()
   -- You can initialize the movement and sprites of various
   -- map entities here.
 end
