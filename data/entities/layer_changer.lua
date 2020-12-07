4df99d1671f39329daec546aceecf3ebf6bb3800
TwilCynder <twilcynder@gmail.com>
2020-12-07 13:40:38 +0100

    Transiion Couche

--- data/entities/layer_changer.lua
+++ data/entities/layer_changer.lua
@@ -1,21 +1,29 @@
--- Lua script of custom entity layer_changer.
--- This script is executed every time a custom entity with this model is created.
-
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation for the full specification
--- of types, events and methods:
--- http://www.solarus-games.org/doc/latest
-
 local entity = ...
 local game = entity:get_game()
 local map = entity:get_map()
 
--- Event called when the custom entity is initialized.
-function entity:on_created()
+function entity:create_top_entity()
+  local x, y, layer = self:get_position()
+  local w, h = self:get_size()
+  local e = map:create_custom_entity({
+    layer = layer + 1,
+    x = x,
+    y = y,
+    width = w,
+    height = h,
+    direction = 0
+  })
 
-  -- Initialize the properties of your custom entity here,
-  -- like the sprite, the size, and whether it can traverse other
-  -- entities and be traversed by them.
+  e:set_modified_ground(self:get_ground_below())
+
+end
+
+function entity:on_created()
+  self:create_top_entity()
+  self:add_collision_test("origin", function(entity, other)
+    if other:get_type() == "hero" then
+      local x, y, layer = other:get_position()
+      other:set_position(x, y, layer + 1)
+    end
+  end)
 end
