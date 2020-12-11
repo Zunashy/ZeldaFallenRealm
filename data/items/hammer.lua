563a192dcc9d18901e113b204fb1afe533a3e227
Bertrand-A <alonso.bertrand@orange.fr>
2020-12-11 16:16:26 +0100

    le marteau il marche mieux

--- data/items/hammer.lua
+++ data/items/hammer.lua
@@ -11,40 +11,33 @@
 local item = ...
 local game = item:get_game()
 local hero = game:get_hero()
-local hammer_sprite
--- local sprite
+local hammer_sprite = sol.sprite.create("hero/hammer")
+
 
 function item:on_created()
   self:set_savegame_variable("possession_hammer")
   self:set_assignable(true)
 end 
 
-
+-- FIN DE l'ITEM
+local function sprite_end_callback(sprite)
+  local hero = game:get_hero()
+  hero:remove_sprite(sprite)  
+  item:set_finished()
+end
 
 -- UTILISATION DE L'ITEM
 function item:on_using()
 
   -- local vars
-  local map = game:get_map()  --utile ??
   local hero = game:get_hero()
   local direction = hero:get_direction() --0:droite 1:haut 2:gauche 3:bas
   local x,y = hero:get_position()
-  local hammer_sprite = sol.sprite.create("hero/hammer") -- comment récup un sprite dans un objet de type item ?
-  
+  local sprite = hero:create_sprite("hero/hammer")
 
-  -- créer l'animation selon la direction de link à +16
-  -- self:set_animation("hammer")
-
-  -- debug
-  print(hammer_sprite)
-
-  item:set_finished()
+  sprite:set_direction(direction)
+  sprite.on_animation_finished = sprite_end_callback
 end
-
 
 function item:on_pickable_created(pickable)
-end
-
-function item:on_obtained()
-  self:set_variant(1)
 end
