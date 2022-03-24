5cb28e0d89633289ccbd97bc622b528373c2baca
TwilCynder <twilcynder@gmail.com>
2022-03-24 14:09:32 +0100

    key counter + fix portes

--- data/scripts/hud/key_counter.lua
+++ data/scripts/hud/key_counter.lua
@@ -8,29 +8,34 @@ local font = image_fonts:new("oracle_red", true)
 local keys_builder = {}
 
 function keys_builder:new(game, config)
-    local menu = {
-        current_count = 9
-    }
+    local menu = {}
+    local small_key = game:get_item("small_key")
+
+    local current_count = small_key:get_amount()
 
     local counter_surface = sol.surface.create(font:get_char_size())
-    counter_surface:fill_color({255, 255, 255})
 
     function menu:on_draw(dst_surface)
-        if self.current_count > 0 then
+        if current_count > 0 then
             sprite:draw(dst_surface, config.x + 4, config.y + 13)
             counter_surface:draw(dst_surface, config.x + 10, config.y + 4)
         end
     end
 
     function menu:redraw()
-        local code = self.current_count + 48
+        counter_surface:clear()
+        local code = current_count + 48
         font:draw_char(code, counter_surface)
     end
 
-    function menu:change_count(count)
-        self.current_count = count
-        self:redraw()
+    local function change_count(item, count)
+        print("on_amount_changed")
+        current_count = count
+        menu:redraw()
     end
+
+    
+    small_key:register_event("on_amount_changed", change_count)
 
     menu:redraw()
 
