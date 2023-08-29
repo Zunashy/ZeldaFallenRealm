60a3b7f0e5138820df40ad82f18ee75920a91fe5
TwilCynder <twilcynder@gmail.com>
2023-08-30 00:46:09 +0200

    pillar work !!

--- data/entities/breakable_pillar.lua
+++ data/entities/breakable_pillar.lua
@@ -29,12 +29,27 @@ function entity:on_created()
     sprite = "entities/destructible/pillar_top"
   })
 
+  entity:set_drawn_in_y_order(true)
+
   self:set_traversable_by(false)
 
 end
 
+local function calc_knockback_angle_h(e, y)
+  local _, ey = e:get_position()
+  return ey < y and math.pi / 2 or math.pi * 1.5
+end
+
+local function calc_knockback_angle_v(e, x)
+  local ex = e:get_position()
+  return ex > x and 0 or math.pi
+end
+
 function entity:on_explosion(bomb)
+  if self.fallen then return end
   print("exploded")
+  self.fallen = true
+
   local angle = bomb:get_angle(self)
   local direction = 0
 
@@ -43,9 +58,42 @@ function entity:on_explosion(bomb)
 
   sprite:set_animation("falling", function()
     sprite:set_animation("fallen")
-    local x, y, layer = self:get_position()
-    x, y = gen.shift_direction4(x, y, direction, 32)
+    entity:set_drawn_in_y_order(false)
+    entity:bring_to_back()
 
+    local x, y, layer = self:get_position()
+
+    local ox, oy = sprite:get_origin()
+
+    local rx = x - ox - (direction == 2 and 32 or 0)
+    local ry = y - oy - (direction == 1 and 32 or 0)
+    local rw = direction % 2 == 0 and 48 or 16
+    local rh = direction % 2 == 1 and 48 or 16
+    for e in map:get_entities_in_rectangle(rx, ry, rw, rh) do
+      if (e:get_type() == "hero") then
+        local kb_angle = direction % 2 == 0 and calc_knockback_angle_h(e, y) or calc_knockback_angle_v(e, x)
+        e:start_hurt(entity, 1)
+
+        local movement = e:get_movement()
+        if movement and movement.set_angle then
+            movement:set_angle(kb_angle)
+            movement:set_ignore_obstacles(true)
+        end
+
+      elseif (e:get_type() == "enemy") then
+        local kb_angle = direction % 2 == 0 and calc_knockback_angle_h(e, y) or calc_knockback_angle_v(e, x)
+
+        e:hurt(1)
+        local kb = sol.movement.create("straight")
+        kb:set_speed(160)
+        kb:set_max_distance(16)
+        kb:set_angle(kb_angle)
+        kb:start(e)
+      end
+    end
+
+
+    x, y = gen.shift_direction4(x, y, direction, 32)
     map:create_destructible({
       x = x,
       y = y,
